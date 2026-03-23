cc716bc0b548900295b89442276fc2bf08eab3f1
Yur Icon <yuricon@gmail.com>
2026-03-23 15:02:21 -0700

    Added plugin created from template

---
+++ playnite-plugin/Gumo/Gumo.cs
@@ -0,0 +1,87 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Gumo
+{
+    public class Gumo : LibraryPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private GumoSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("fd666191-5124-4c16-8b9b-0a841193822c");
+
+        // Change to something more appropriate
+        public override string Name => "Custom Library";
+
+        // Implementing Client adds ability to open it via special menu in playnite.
+        public override LibraryClient Client { get; } = new GumoClient();
+
+        public Gumo(IPlayniteAPI api) : base(api)
+        {
+            settings = new GumoSettingsViewModel(this);
+            Properties = new LibraryPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override IEnumerable<GameMetadata> GetGames(LibraryGetGamesArgs args)
+        {
+            // Return list of user's games.
+            return new List<GameMetadata>()
+            {
+                new GameMetadata()
+                {
+                    Name = "Notepad",
+                    GameId = "notepad",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "notepad.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"c:\Windows\notepad.exe")
+                },
+                new GameMetadata()
+                {
+                    Name = "Calculator",
+                    GameId = "calc",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "calc.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"https://playnite.link/applogo.png"),
+                    BackgroundImage = new MetadataFile(@"https://playnite.link/applogo.png")
+                }
+            };
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new GumoSettingsView();
+        }
+    }
+}
---
+++ playnite-plugin/Gumo/GumoClient.cs
@@ -0,0 +1,19 @@
+﻿using Playnite.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gumo
+{
+    public class GumoClient : LibraryClient
+    {
+        public override bool IsInstalled => true;
+
+        public override void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
